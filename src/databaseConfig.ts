f17ef2ebcb72a7254139945dd1134093af02aa69
Markus-Scl <76035547+Markus-Scl@users.noreply.github.com>
2025-02-25 15:44:21 +0100

    started server setup

---
+++ src/databaseConfig.ts
@@ -0,0 +1,17 @@
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST_URL || "localhost",
+    port:  process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "1234",
+    database: process.env.DB_NAME || "curaiNotes",
+    synchronize: true, // Set to false in production
+    logging: true,
+    entities: ["src/entities/*.ts"],
+    subscribers: [],
+    migrations: [],
+})
+
+export default dataSource;
