1211e3e38cf75fcadfdd851a31a205ae91cee635
Markus-Scl <76035547+Markus-Scl@users.noreply.github.com>
2025-02-25 16:45:08 +0100

    restructured index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import express from "express";
-import dataSource from "./databaseConfig";
-import {createServer} from 'http';
-import cors from 'cors';
+import { createServer } from "http";
+import cors from "cors";
+import dataSource from "./databaseConfig"; // Ensure this path is correct
 
 const app = express();
 const httpServer = createServer(app);
 const port = 3000;
 
-dataSource.initialize()
-    .then(async () => {
-        console.log("Database connected");
-    })
-    .catch((error) => console.log("Database connection error:", error));
-
-
 app.use(express.json());
 
 app.use(
-	cors({
-		credentials: true,
-		origin: ['http://localhost:3000', 'http://localhost:3001'],
-		allowedHeaders: ['Content-Type', 'accesstoken', 'refreshtoken'], // Allowed headers that can be sent by the client
-		exposedHeaders: ['newaccesstoken'], // Allowed headers that can be accessed by the client
-	})
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    allowedHeaders: ["Content-Type", "accesstoken", "refreshtoken"],
+    exposedHeaders: ["newaccesstoken"],
+  })
 );
 
+dataSource
+  .initialize()
+  .then(async () => {
+    console.log("Database connected");
+  })
+  .catch((error) => console.log("Database connection error:", error));
+
 httpServer.listen(port, () => {
-	console.log('Server running on port', port);
+  console.log("Server running on port", port);
 });
